2824bed555ee532c9ca3543ba5efa874a89d0913
Mitchell Wood <WOODM@bridgwater.ac.uk>
2026-02-26 14:51:31 +0000

    Can now add and view films

--- CinemaWebsite/Areas/Staff/Controllers/StaffDashboardController.cs
+++ CinemaWebsite/Areas/Staff/Controllers/StaffDashboardController.cs
@@ -33,6 +33,37 @@ namespace CinemaWebsite.Areas.Staff.Controllers
             {
                 //Save the image to the server and save the file path to the database.
 
+                //Define the file path 
+                var imageDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/films");
+
+                if (!Directory.Exists(imageDirectory))
+                {
+                    Directory.CreateDirectory(imageDirectory);
+                }
+
+                //Combine the directory with the image name
+                var filePath = Path.Combine(imageDirectory, imageFile.FileName);
+
+                //try to save the image to the server
+                try
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await imageFile.CopyToAsync(stream);
+                    }
+
+                    //Save the image path to the database
+                    film.Image = "/images/films/" + imageFile.FileName;
+
+                    _context.Films.Add(film);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                catch(Exception ex)
+                {
+                    ModelState.AddModelError("", "Please select an image file");
+                    return View("Index", film);
+                }
             }
             else
             {
---
+++ CinemaWebsite/Controllers/FilmController.cs
@@ -0,0 +1,23 @@
+﻿using CinemaWebsite.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaWebsite.Controllers
+{
+    public class FilmController : Controller
+    {
+        //Inject the database
+        private readonly ApplicationDbContext _context;
+        public FilmController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            //Gather all the films
+            var films = _context.Films.ToList();
+            return View(films);
+        }
+    }
+}
